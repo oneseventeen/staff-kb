9595b4406eab5df6967c5d248293874d8cd98d97
Chris Adams <oneseventeen@gmail.com>
2025-11-12 22:34:39 -0700

    Initial UI additions and layout

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="border-b border-gray-200 bg-white py-4 px-6">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-2xl font-bold text-gray-900">Staff KB</h1>
+      </div>
+    </header>
+  );
+}
